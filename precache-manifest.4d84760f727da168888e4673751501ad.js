968517e3d5cbf05ca623dc06411c03aa6bad8c1b
Eileen Hays-Schwantes <ehaysschwantes@lyft.com>
2019-09-28 16:05:14 -0700

    Updates

---
+++ precache-manifest.4d84760f727da168888e4673751501ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f75fa188e3d3b9ad373bddc694b8a2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "39a29e68e35aacf292b2",
+    "url": "/monsters-rolodex/static/css/main.7e6c27d7.chunk.css"
+  },
+  {
+    "revision": "8a5ff6dc5e1ca644807f",
+    "url": "/monsters-rolodex/static/js/2.12729fbf.chunk.js"
+  },
+  {
+    "revision": "39a29e68e35aacf292b2",
+    "url": "/monsters-rolodex/static/js/main.31befbce.chunk.js"
+  },
+  {
+    "revision": "78aec4e4b283e3de3c0f",
+    "url": "/monsters-rolodex/static/js/runtime~main.c620570a.js"
+  }
+]);
